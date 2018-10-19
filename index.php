520bd5ba34612e4ac3c07cdd6b9a0c2f552b936a
ganargatul <ganar.gatul@gmail.com>
2018-10-19 10:08:49 +0700

    first

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+	include "koneksi.php";
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data</title>
+</head>
+<body>
+	<table align="center" border="1" width="70%">
+		<tr>
+			<td colspan="5">
+				<h3><center>DATA</center></h3>
+			</td>
+		</tr>
+		<tr>
+			<th>id</th>
+			<th>username</th>
+			<th>password</th>
+			<th>level</th>
+			<th>fullname</th>
+		</tr>
+			<?php 
+
+				$qry = mysqli_query($koneksi,"SELECT * FROM users");
+				while($data = mysqli_fetch_array($qry)){
+			 ?>
+		<tr>
+			
+			<td><?php echo $data['id']; ?></td>
+			<td><?php echo $data['username']; ?></td>
+			<td><?php echo $data['password']; ?></td>
+			<td><?php echo $data['level']; ?></td>
+            <td><?php echo $data['fullname']; ?></td>
+		</tr>
+
+	<?php  
+}
+?>
+	</table>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php 
+    $host="localhost";
+    $username ="root";
+    $password = "";
+    $database = "php-api";
+
+    $koneksi = mysqli_connect($host,$username,$password,$database);
+    
+
+
+?>
